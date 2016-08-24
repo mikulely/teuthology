f86a568028be905a7b94bb78dc7ef3f6edafe801
Vasu Kulkarni <vasu@redhat.com>
2016-08-24 12:05:22 -0700

    rh specific install changes
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- teuthology/task/install.py
+++ teuthology/task/install.py
@@ -3,7 +3,6 @@ from cStringIO import StringIO
 import contextlib
 import copy
 import logging
-import time
 import os
 import subprocess
 import yaml
@@ -702,121 +701,143 @@ def rh_install(ctx, config):
     :param ctx: the argparse.Namespace object
     :param config: the config dict
     """
+    yaml_path = None
+    # Look for rh specific packages in <suite_path>/rh/downstream.yaml
+    if 'suite_path' in ctx.config:
+        ds_yaml = os.path.join(
+            ctx.config['suite_path'],
+            'rh',
+            'downstream.yaml',
+        )
+        if os.path.exists(ds_yaml):
+            yaml_path = ds_yaml
+    # default to user home dir if one exists
+    default_yaml = os.path.expanduser('~/downstream.yaml')
+    if os.path.exists(default_yaml):
+        yaml_path = default_yaml
+    rh_ds_yaml = yaml.safe_load(open(yaml_path))
+    rh_versions = rh_ds_yaml.get('versions').get('supported')
     version = config['rhbuild']
-    rh_versions = ['1.3.0', '1.3.1', '1.3.2', '2.0']
     if version in rh_versions:
         log.info("%s is a supported version", version)
     else:
         raise RuntimeError("Unsupported RH Ceph version %s", version)
-
     with parallel() as p:
         for remote in ctx.cluster.remotes.iterkeys():
             if remote.os.name == 'rhel':
                 log.info("Installing on RHEL node: %s", remote.shortname)
-                p.spawn(rh_install_pkgs, ctx, remote, version)
+                p.spawn(rh_install_pkgs, ctx, remote, version, rh_ds_yaml)
             else:
-                log.info("Node %s is not RHEL", remote.shortname)
-                raise RuntimeError("Test requires RHEL nodes")
+                log.info("Install on Ubuntu node: %s", remote.shortname)
+                p.spawn(rh_install_deb_pkgs, ctx, remote, version, rh_ds_yaml,
+                        config['deb-repo'], config['deb-gpg-key'])
     try:
         yield
     finally:
         if config.get('skip_uninstall'):
             log.info("Skipping uninstall of Ceph")
         else:
-            rh_uninstall(ctx=ctx, config=config)
+            with parallel() as p:
+                for remote in ctx.cluster.remotes.iterkeys():
+                    p.spawn(rh_uninstall_pkgs, ctx, remote, rh_ds_yaml)
 
 
-def rh_uninstall(ctx, config):
+def rh_install_deb_pkgs(ctx, remote, version, rh_ds_yaml, deb_repo, deb_gpg_key):
     """
-     Uninstalls rh ceph on all hosts.
-     It actually spawns rh_uninstall_pkgs() on the remotes for uninstall.
-
-    :param ctx: the argparse.Namespace object
-    :param config: the config dict
+    Setup debian repo, Install gpg key
+    and Install on debian packages
+    : param ctx
+    : param remote
     """
-    with parallel() as p:
-        for remote in ctx.cluster.remotes.iterkeys():
-            p.spawn(rh_uninstall_pkgs, ctx, remote)
+    set_rh_deb_repo(remote, deb_repo, deb_gpg_key)
+    rh_version_check = rh_ds_yaml.get('versions').get('deb').get('mapped')
+    rh_deb_pkgs = rh_ds_yaml.get('pkgs').get('deb')
+    pkgs = str.join(' ', rh_deb_pkgs)
+    log.info("Installing redhat ceph packages")
+    remote.run(args=['sudo', 'apt-get', '-y', 'install',
+                     run.Raw(pkgs)])
+    # check package version
+    installed_version = packaging.get_package_version(remote, 'ceph-common')
+    log.info("Node: {n} Ceph version installed is {v}".format(n=remote.shortname, v=version))
+    req_ver = rh_version_check[version]
+    if installed_version.startswith(req_ver):
+        log.info("Installed version matches on %s", remote.shortname)
+    else:
+        raise RuntimeError("Version check failed on node %s", remote.shortname)
 
 
-def rh_install_pkgs(ctx, remote, installed_version):
+def set_rh_deb_repo(remote, deb_repo, deb_gpg_key=None):
+    """
+    Sets up debian repo and gpg key for package verification
+    """
+    repos = ['MON', 'OSD', 'Tools']
+    log.info("deb repo: %s", deb_repo)
+    log.info("gpg key url: %s", deb_gpg_key)
+    remote.run(args=['sudo', 'rm', '-f', run.Raw('/etc/apt/sources.list.d/*')],
+               check_status=False)
+    for repo in repos:
+        cmd = 'echo deb ' + deb_repo + '/{0}'.format(repo) + \
+              ' $(lsb_release -sc) main'
+        remote.run(args=['sudo', run.Raw(cmd), run.Raw('>'),
+                         "/tmp/{0}.list".format(repo)])
+        remote.run(args=['sudo', 'cp', "/tmp/{0}.list".format(repo),
+                         '/etc/apt/sources.list.d/'])
+    # add ds gpgkey
+    ds_keys = ['https://www.redhat.com/security/897da07a.txt',
+               'https://www.redhat.com/security/f21541eb.txt']
+    if deb_gpg_key is not None:
+        ds_keys.append(deb_gpg_key)
+    for key in ds_keys:
+        wget_cmd = 'wget -O - ' + key
+        remote.run(args=['sudo', run.Raw(wget_cmd),
+                         run.Raw('|'), 'sudo', 'apt-key', 'add', run.Raw('-')])
+    remote.run(args=['sudo', 'apt-get', 'update'])
+
+
+def rh_install_pkgs(ctx, remote, version, rh_ds_yaml):
     """
     Installs RH build using ceph-deploy.
 
     :param ctx: the argparse.Namespace object
     :param remote: the teuthology.orchestra.remote.Remote object
     """
-    pkgs = ['ceph-deploy']
-    # install ceph-selinux for 1.3.2 as its not dependency of any core packages
-    if (installed_version == '1.3.2'):
-        pkgs.append('ceph-selinux')
-    # install ceph-fuse for 2.0 as its not dependency of any core packages
-    if (installed_version == '2.0'):
-        pkgs.append('ceph-fuse')
-    rh_version_check = {'0.94.1': '1.3.0', '0.94.3': '1.3.1',
-                        '0.94.5': '1.3.2', '10.1.0': '2.0'}
+    rh_version_check = rh_ds_yaml.get('versions').get('rpm').get('mapped')
+    rh_rpm_pkgs = rh_ds_yaml.get('pkgs').get('rpm')
+    pkgs = str.join(' ', rh_rpm_pkgs)
     log.info("Remove any epel packages installed on node %s", remote.shortname)
-    remote.run(args=['sudo', 'yum', 'remove', run.Raw("leveldb xmlstarlet fcgi"), '-y'],check_status=False)
-    for pkg in pkgs:
-        log.info("Check if %s is already installed on node %s", pkg, remote.shortname)
-        remote.run(args=['sudo', 'yum', 'clean', 'metadata'])
-        r = remote.run(
-             args=['yum', 'list', 'installed', run.Raw(pkg)],
-             stdout=StringIO(),
-             check_status=False,
-            )
-        if r.stdout.getvalue().find(pkg) == -1:
-            log.info("Installing %s " % pkg)
-            remote.run(args=['sudo', 'yum', 'install', pkg, '-y'])
-        else:
-            log.info("Removing and reinstalling %s on %s", pkg, remote.shortname)
-            remote.run(args=['sudo', 'yum', 'remove', pkg, '-y'])
-            remote.run(args=['sudo', 'yum', 'install', pkg, '-y'])
-
-    log.info("Check if ceph is already installed on %s", remote.shortname)
-    r = remote.run(
-          args=['yum', 'list', 'installed','ceph'],
-          stdout=StringIO(),
-          check_status=False,
-        )
-    host = r.hostname
-    if r.stdout.getvalue().find('ceph') == -1:
-        log.info("Install ceph using ceph-deploy on %s", remote.shortname)
-        remote.run(args=['sudo', 'ceph-deploy', 'install', run.Raw('--no-adjust-repos'), host])
-        remote.run(args=['sudo', 'yum', 'install', 'ceph-test', '-y'])
-    else:
-        log.info("Removing and reinstalling Ceph on %s", remote.shortname)
-        remote.run(args=['sudo', 'ceph-deploy', 'uninstall', host])
-        remote.run(args=['sudo', 'ceph-deploy', 'purgedata', host])
-        remote.run(args=['sudo', 'ceph-deploy', 'install', host])
-        remote.run(args=['sudo', 'yum', 'remove', 'ceph-test', '-y'])
-        remote.run(args=['sudo', 'yum', 'install', 'ceph-test', '-y'])
-
+    remote.run(args=['sudo', 'yum', 'remove', run.Raw("leveldb xmlstarlet fcgi"), '-y'],
+               check_status=False)
+    log.info("Installing redhat ceph packages")
+    remote.run(args=['sudo', 'yum', '-y', 'install',
+                     run.Raw(pkgs)])
     # check package version
-    version = packaging.get_package_version(remote, 'ceph-common')
-    log.info("Node: {n} Ceph version installed is {v}".format(n=remote.shortname,v=version))
+    installed_version = packaging.get_package_version(remote, 'ceph-common')
+    log.info("Node: {n} Ceph version installed is {v}".format(n=remote.shortname,
+                                                              v=version))
     if rh_version_check[version] == installed_version:
         log.info("Installed version matches on %s", remote.shortname)
     else:
         raise RuntimeError("Version check failed on node %s", remote.shortname)
 
 
-def rh_uninstall_pkgs(ctx, remote):
+def rh_uninstall_pkgs(ctx, remote, rh_ds_yaml):
     """
     Removes Ceph from all RH hosts
 
     :param ctx: the argparse.Namespace object
     :param remote: the teuthology.orchestra.remote.Remote object
     """
-    log.info("uninstalling packages using ceph-deploy on node %s", remote.shortname)
-    r = remote.run(args=['date'], check_status=False)
-    host = r.hostname
-    remote.run(args=['sudo', 'ceph-deploy', 'uninstall', host])
-    time.sleep(4)
-    remote.run(args=['sudo', 'ceph-deploy', 'purgedata', host])
-    log.info("Uninstalling ceph-deploy")
-    remote.run(args=['sudo', 'yum', 'remove', 'ceph-deploy', '-y'], check_status=False)
-    remote.run(args=['sudo', 'yum', 'remove', 'ceph-test', '-y'], check_status=False)
+    rh_rpm_pkgs = rh_ds_yaml.get('pkgs').get('rpm')
+    rpm_pkgs = str.join(' ', rh_rpm_pkgs)
+
+    rh_deb_pkgs = rh_ds_yaml.get('pkgs').get('deb')
+    deb_pkgs = str.join(' ', rh_deb_pkgs)
+
+    if remote.os.name == 'rhel':
+        remote.run(args=['sudo', 'yum', 'remove', run.Raw(rpm_pkgs), '-y'])
+    else:
+        remote.run(args=['sudo', 'apt-get', 'remove', run.Raw(deb_pkgs), '-y'])
+    remote.run(args=['sudo', 'rm', '-rf', '/var/lib/ceph'])
 
 
 def _upgrade_rpm_packages(ctx, config, remote, pkgs):
